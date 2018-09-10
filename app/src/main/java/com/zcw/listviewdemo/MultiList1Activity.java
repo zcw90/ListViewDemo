e6a9dd29d809fb3db785b73ba8b8cafca62a7ea0
zhuchengwei <chengwei493>
2018-09-10 16:07:57 +0800

    1、add multi choice.

--- app/src/main/java/com/zcw/listviewdemo/MultiList1Activity.java
+++ app/src/main/java/com/zcw/listviewdemo/MultiList1Activity.java
@@ -1,22 +1,26 @@
 package com.zcw.listviewdemo;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.SparseBooleanArray;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.zcw.base.CommonUtils;
+import com.zcw.base.view.CustomDialog;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,9 +33,12 @@ public class MultiList1Activity extends AppCompatActivity {
     private List<String> data;
 
     private LinearLayout llMultiChoiceTitle;
-    private Button btnDelete;
+    private TextView tvCancel;
+    private TextView tvChoiceCount;
+    private TextView tvChoiceAllOrNone;
+    private ImageView imgDelete;
 
-    private TextView tvSelectResult;
+    private TextView tvChoiceResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +50,12 @@ public class MultiList1Activity extends AppCompatActivity {
 
     private void init() {
         llMultiChoiceTitle = findViewById(R.id.ll_multi_title1);
-        btnDelete = findViewById(R.id.btn_multi_delete1);
-        tvSelectResult = findViewById(R.id.tv_multi_select_result1);
+        tvCancel = llMultiChoiceTitle.findViewById(R.id.tv_multi_choice_cancel1);
+        tvChoiceCount = llMultiChoiceTitle.findViewById(R.id.tv_multi_choice_count1);
+        tvChoiceAllOrNone = llMultiChoiceTitle.findViewById(R.id.tv_multi_choice_all_none1);
+        imgDelete = findViewById(R.id.img_multi_delete1);
+        tvChoiceResult = findViewById(R.id.tv_multi_choice_result1);
+        tvChoiceResult.setMovementMethod(ScrollingMovementMethod.getInstance());
 
         data = new ArrayList<>();
         for(int i = 0; i < 50; i++) {
@@ -58,18 +69,18 @@ public class MultiList1Activity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                CommonUtils.toast(MultiList1Activity.this, data.get(position) + " Selection!!!");
+                CommonUtils.toast(MultiList1Activity.this, data.get(position) + " Choice!!!");
 
-                if(listView.getChoiceMode() == AbsListView.CHOICE_MODE_MULTIPLE) {
-                    SparseBooleanArray booleanArray = listView.getCheckedItemPositions();
-                    StringBuilder builder = new StringBuilder();
-                    for(int i = 0;i < booleanArray.size(); i++) {
-                        int key = booleanArray.keyAt(i);
-                        if(booleanArray.get(key)) {
-                            builder.append(adapter.getItem(key) + "\n");
-                        }
+                if(adapter.isChoiceMulti()) {
+                    showChoiceResult();
+
+                    // 设置按钮全选/全不选
+                    if(listView.getCheckedItemCount() == adapter.getCount()) {
+                        tvChoiceAllOrNone.setText(R.string.choice_none);
                     }
-                    tvSelectResult.setText(builder.toString());
+                    else {
+                        tvChoiceAllOrNone.setText(R.string.choice_all);
+                    }
                 }
             }
         });
@@ -77,14 +88,159 @@ public class MultiList1Activity extends AppCompatActivity {
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                if(listView.getChoiceMode() != AbsListView.CHOICE_MODE_MULTIPLE) {
-                    listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
-                    adapter.setSelectMulti(true);
-                }
-                adapter.notifyDataSetChanged();
+                CommonUtils.toast(MultiList1Activity.this, "Long click " + data.get(position));
+
+                enterMultiChoice(position);
                 return true;
             }
         });
+
+        tvCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exitMultiChoice();
+            }
+        });
+
+        tvChoiceAllOrNone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(tvChoiceAllOrNone.getText().toString().equals(getString(R.string.choice_all))) {
+                    tvChoiceAllOrNone.setText(R.string.choice_none);
+
+                    // 设置全选
+                    for(int i = 0; i < adapter.getCount(); i++) {
+                        listView.setItemChecked(i, true);
+                    }
+                }
+                else {
+                    tvChoiceAllOrNone.setText(R.string.choice_all);
+
+                    // 设置全不选
+                    listView.clearChoices();
+                }
+
+                showChoiceResult();
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        imgDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(listView.getCheckedItemCount() == 0) {
+                    CommonUtils.toast(MultiList1Activity.this, "请选择要删除的项");
+                    return ;
+                }
+
+                final CustomDialog dialog = new CustomDialog(MultiList1Activity.this);
+                dialog.withButton1("确定", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+
+                        // 删除选中的项
+                        List<String> deleteData = new ArrayList<>();
+                        SparseBooleanArray booleanArray = listView.getCheckedItemPositions();
+                        for(int i = 0;i < booleanArray.size(); i++) {
+                            int key = booleanArray.keyAt(i);
+                            if(booleanArray.get(key)) {
+                                deleteData.add(adapter.getItem(key));
+                            }
+                        }
+
+                        listView.clearChoices();
+                        data.removeAll(deleteData);
+
+                        exitMultiChoice();
+                    }
+                });
+                dialog.withButton2("取消", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.withMessage(String.format(getString(R.string.delete_hint), listView.getCheckedItemCount()));
+                dialog.show();
+            }
+        });
+    }
+
+    /**
+     * 进入多选
+     * @param position 默认选中的项
+     * @return 由非多选进入多选，返回true；否在返回false。
+     */
+    private boolean enterMultiChoice(int position) {
+        if(!adapter.isChoiceMulti()) {
+            listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
+            listView.clearChoices();
+
+            if(position >= 0 && position < adapter.getCount()) {
+                listView.setItemChecked(position, true);
+            }
+            adapter.setChoiceMulti(true);
+            adapter.notifyDataSetChanged();
+
+            llMultiChoiceTitle.setVisibility(View.VISIBLE);
+            imgDelete.setVisibility(View.VISIBLE);
+            tvChoiceAllOrNone.setText(R.string.choice_all);
+            showChoiceResult();
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 退出多选
+     * @return 如果是由多选退出到非多选，返回true；否在返回false。
+     */
+    private boolean exitMultiChoice() {
+        if(adapter.isChoiceMulti()) {
+            tvChoiceResult.setText("");
+            llMultiChoiceTitle.setVisibility(View.GONE);
+            imgDelete.setVisibility(View.GONE);
+
+            listView.clearChoices();
+            adapter.setChoiceMulti(false);
+            adapter.notifyDataSetChanged();
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    listView.setChoiceMode(AbsListView.CHOICE_MODE_NONE);
+                }
+            }, 10);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 显示选择结果
+     */
+    private void showChoiceResult() {
+        SparseBooleanArray booleanArray = listView.getCheckedItemPositions();
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0;i < booleanArray.size(); i++) {
+            int key = booleanArray.keyAt(i);
+            if(booleanArray.get(key)) {
+                builder.append(adapter.getItem(key) + "\n");
+            }
+        }
+
+        tvChoiceCount.setText(String.format(getString(R.string.choice_count), listView.getCheckedItemCount()));
+        tvChoiceResult.setText(builder.toString());
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK) {
+            if(exitMultiChoice())
+                return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
     private static class Multi1Adapter extends BaseAdapter {
@@ -93,20 +249,20 @@ public class MultiList1Activity extends AppCompatActivity {
         private List<String> data;
 
         /** 是否进入多选模式 */
-        private boolean isSelectMulti;
+        private boolean isChoiceMulti;
 
         public Multi1Adapter(Context context, List<String> data) {
             this.context = context;
             this.data = data;
-            this.isSelectMulti = false;
+            this.isChoiceMulti = false;
         }
 
-        public boolean isSelectMulti() {
-            return isSelectMulti;
+        public boolean isChoiceMulti() {
+            return isChoiceMulti;
         }
 
-        public void setSelectMulti(boolean selectMulti) {
-            isSelectMulti = selectMulti;
+        public void setChoiceMulti(boolean choiceMulti) {
+            isChoiceMulti = choiceMulti;
         }
 
         @Override
@@ -134,7 +290,7 @@ public class MultiList1Activity extends AppCompatActivity {
                 view = LayoutInflater.from(context).inflate(R.layout.item_multi_list1, parent, false);
 
                 holder.tvContent = view.findViewById(R.id.tv_multi_item_content1);
-                holder.imgSelect = view.findViewById(R.id.img_multi_item_content1);
+                holder.imgChecked = view.findViewById(R.id.img_multi_item_content1);
                 view.setTag(holder);
             }
             else {
@@ -142,14 +298,14 @@ public class MultiList1Activity extends AppCompatActivity {
             }
 
             holder.tvContent.setText(data.get(position));
-            holder.imgSelect.setVisibility(isSelectMulti ? View.VISIBLE : View.GONE);
+            holder.imgChecked.setVisibility(isChoiceMulti ? View.VISIBLE : View.GONE);
 
             return view;
         }
 
         private static class ViewHolder {
             public TextView tvContent;
-            public ImageView imgSelect;
+            public ImageView imgChecked;
         }
     }
 }
--- app/src/main/java/com/zcw/listviewdemo/MultiList2Activity.java
+++ app/src/main/java/com/zcw/listviewdemo/MultiList2Activity.java
@@ -1,22 +1,26 @@
 package com.zcw.listviewdemo;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.SparseBooleanArray;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.zcw.base.CommonUtils;
+import com.zcw.base.view.CustomDialog;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,9 +33,12 @@ public class MultiList2Activity extends AppCompatActivity {
     private List<String> data;
 
     private LinearLayout llMultiChoiceTitle;
-    private Button btnDelete;
+    private TextView tvCancel;
+    private TextView tvChoiceCount;
+    private TextView tvChoiceAllOrNone;
+    private ImageView imgDelete;
 
-    private TextView tvSelectResult;
+    private TextView tvChoiceResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +50,12 @@ public class MultiList2Activity extends AppCompatActivity {
 
     private void init() {
         llMultiChoiceTitle = findViewById(R.id.ll_multi_title2);
-        btnDelete = findViewById(R.id.btn_multi_delete2);
-        tvSelectResult = findViewById(R.id.tv_multi_select_result2);
+        tvCancel = llMultiChoiceTitle.findViewById(R.id.tv_multi_choice_cancel2);
+        tvChoiceCount = llMultiChoiceTitle.findViewById(R.id.tv_multi_choice_count2);
+        tvChoiceAllOrNone = llMultiChoiceTitle.findViewById(R.id.tv_multi_choice_all_none2);
+        imgDelete = findViewById(R.id.img_multi_delete2);
+        tvChoiceResult = findViewById(R.id.tv_multi_choice_result2);
+        tvChoiceResult.setMovementMethod(ScrollingMovementMethod.getInstance());
 
         data = new ArrayList<>();
         for(int i = 0; i < 50; i++) {
@@ -52,36 +63,205 @@ public class MultiList2Activity extends AppCompatActivity {
         }
 
         listView = findViewById(R.id.lv_multi_test2);
-        listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
         adapter = new Multi2Adapter(this, data);
         listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                CommonUtils.toast(MultiList2Activity.this, data.get(position) + " Selection!!!");
+                CommonUtils.toast(MultiList2Activity.this, data.get(position) + " Choice!!!");
 
-                SparseBooleanArray booleanArray = listView.getCheckedItemPositions();
-                StringBuilder builder = new StringBuilder();
-                for(int i = 0;i < booleanArray.size(); i++) {
-                    int key = booleanArray.keyAt(i);
-                    if(booleanArray.get(key)) {
-                        builder.append(adapter.getItem(key) + "\n");
+                if(adapter.isChoiceMulti()) {
+                    showChoiceResult();
+
+                    // 设置按钮全选/全不选
+                    if(listView.getCheckedItemCount() == adapter.getCount()) {
+                        tvChoiceAllOrNone.setText(R.string.choice_none);
+                    }
+                    else {
+                        tvChoiceAllOrNone.setText(R.string.choice_all);
                     }
                 }
-                tvSelectResult.setText(builder.toString());
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                CommonUtils.toast(MultiList2Activity.this, "Long click " + data.get(position));
+
+                enterMultiChoice(position);
+                return true;
+            }
+        });
+
+        tvCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exitMultiChoice();
+            }
+        });
+
+        tvChoiceAllOrNone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(tvChoiceAllOrNone.getText().toString().equals(getString(R.string.choice_all))) {
+                    tvChoiceAllOrNone.setText(R.string.choice_none);
+
+                    // 设置全选
+                    for(int i = 0; i < adapter.getCount(); i++) {
+                        listView.setItemChecked(i, true);
+                    }
+                }
+                else {
+                    tvChoiceAllOrNone.setText(R.string.choice_all);
+
+                    // 设置全不选
+                    listView.clearChoices();
+                }
+
+                showChoiceResult();
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        imgDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(listView.getCheckedItemCount() == 0) {
+                    CommonUtils.toast(MultiList2Activity.this, "请选择要删除的项");
+                    return ;
+                }
+
+                final CustomDialog dialog = new CustomDialog(MultiList2Activity.this);
+                dialog.withButton1("确定", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+
+                        // 删除选中的项
+                        List<String> deleteData = new ArrayList<>();
+                        SparseBooleanArray booleanArray = listView.getCheckedItemPositions();
+                        for(int i = 0;i < booleanArray.size(); i++) {
+                            int key = booleanArray.keyAt(i);
+                            if(booleanArray.get(key)) {
+                                deleteData.add(adapter.getItem(key));
+                            }
+                        }
+
+                        listView.clearChoices();
+                        data.removeAll(deleteData);
+
+                        exitMultiChoice();
+                    }
+                });
+                dialog.withButton2("取消", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.withMessage(String.format(getString(R.string.delete_hint), listView.getCheckedItemCount()));
+                dialog.show();
             }
         });
     }
 
-    private static class Multi2Adapter extends BaseAdapter {
+    /**
+     * 进入多选
+     * @param position 默认选中的项
+     * @return 由非多选进入多选，返回true；否在返回false。
+     */
+    private boolean enterMultiChoice(int position) {
+        if(!adapter.isChoiceMulti()) {
+            listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
+            listView.clearChoices();
 
+            if(position >= 0 && position < adapter.getCount()) {
+                listView.setItemChecked(position, true);
+            }
+            adapter.setChoiceMulti(true);
+            adapter.notifyDataSetChanged();
+
+            llMultiChoiceTitle.setVisibility(View.VISIBLE);
+            imgDelete.setVisibility(View.VISIBLE);
+            tvChoiceAllOrNone.setText(R.string.choice_all);
+            showChoiceResult();
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 退出多选
+     * @return 如果是由多选退出到非多选，返回true；否在返回false。
+     */
+    private boolean exitMultiChoice() {
+        if(adapter.isChoiceMulti()) {
+            tvChoiceResult.setText("");
+            llMultiChoiceTitle.setVisibility(View.GONE);
+            imgDelete.setVisibility(View.GONE);
+
+            listView.clearChoices();
+            adapter.setChoiceMulti(false);
+            adapter.notifyDataSetChanged();
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    listView.setChoiceMode(AbsListView.CHOICE_MODE_NONE);
+                }
+            }, 10);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 显示选择结果
+     */
+    private void showChoiceResult() {
+        SparseBooleanArray booleanArray = listView.getCheckedItemPositions();
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0;i < booleanArray.size(); i++) {
+            int key = booleanArray.keyAt(i);
+            if(booleanArray.get(key)) {
+                builder.append(adapter.getItem(key) + "\n");
+            }
+        }
+
+        tvChoiceCount.setText(String.format(getString(R.string.choice_count), listView.getCheckedItemCount()));
+        tvChoiceResult.setText(builder.toString());
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK) {
+            if(exitMultiChoice())
+                return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private static class Multi2Adapter extends BaseAdapter {
         private Context context;
         private List<String> data;
+
+        /** 是否进入多选模式 */
+        private boolean isChoiceMulti;
 
         public Multi2Adapter(Context context, List<String> data) {
             this.context = context;
             this.data = data;
+            this.isChoiceMulti = false;
+        }
+
+        public boolean isChoiceMulti() {
+            return isChoiceMulti;
+        }
+
+        public void setChoiceMulti(boolean choiceMulti) {
+            isChoiceMulti = choiceMulti;
         }
 
         @Override
@@ -109,6 +289,7 @@ public class MultiList2Activity extends AppCompatActivity {
                 view = LayoutInflater.from(context).inflate(R.layout.item_multi_list2, parent, false);
 
                 holder.tvContent = view.findViewById(R.id.tv_multi_item_content2);
+                holder.imgChecked = view.findViewById(R.id.img_multi_item_content2);
                 view.setTag(holder);
             }
             else {
@@ -116,12 +297,13 @@ public class MultiList2Activity extends AppCompatActivity {
             }
 
             holder.tvContent.setText(data.get(position));
+            holder.imgChecked.setVisibility(isChoiceMulti ? View.VISIBLE : View.GONE);
             return view;
         }
 
         private static class ViewHolder {
             public TextView tvContent;
-            public ImageView imgSelect;
+            public ImageView imgChecked;
         }
     }
 }
